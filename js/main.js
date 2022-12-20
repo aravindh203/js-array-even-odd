0f1fcf6dbd4ac9f696cd1323ac310995aa00e56b
aravindh203 <aravindhkumar218@gmail.com>
2022-12-20 22:00:34 +0530

    add

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 let array=[33,27,26,44,187,77,82,88,109,102,115,66,82,13,12];
-console.log(array)
+console.log(array);
 let b=0;
 let c=0;
 let even=[];
 let odd=[];
 for(i=0;i<array.length;i++){
     if(array[i]%2==0){
-        b
+        
         even[b]=array[i];
-        b++
+        b++;
     }
     else{
-        c
+        
         odd[c]=array[i];
-        c++
+        c++;
     }
 }
 console.log(even);
